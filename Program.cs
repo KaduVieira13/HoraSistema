b5ea4d4665a92bec6d7cab8e2f36576ef3c5da12
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-25 11:13:54 -0300

    Exibição de hora e data do sistema

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HoraSistema
+{
+              class Program
+              {
+                            static void Main(string[] args)
+                            {
+                                          DateTime horaSistema = DateTime.Now;
+                                          string horaLonga = horaSistema.ToLongTimeString();
+                                          string datalonga = horaSistema.ToLongDateString();
+
+
+                                          Console.WriteLine("--- Exibindo a hora deste computador --- \n");
+                                          Console.WriteLine($"Olá, hoje é: {horaSistema}");
+                                          Console.WriteLine($"Ou seja:  {datalonga}");
+                                          Console.WriteLine($"E são exatamente: {horaLonga}");
+                            }
+              }
+}
